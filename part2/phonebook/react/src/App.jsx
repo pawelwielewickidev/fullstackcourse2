40a8e6517c799c1fcc974aaed345bb690e846902
pawelwielewickidev <pawel.wielewicki.dev@gmail.com>
2026-04-07 10:00:18 +0200

    2.8

--- part2/phonebook/react/src/App.jsx
+++ part2/phonebook/react/src/App.jsx
@@ -5,22 +5,28 @@ const Display = (props) => {
   return (
     <ul>
       {props.persons.map((person, index) => (
-        <Person key={index} name={person.name} />
+        <Person key={index} name={person.name} number={person.number} />
       ))}
     </ul>
   );
 };
 
 const Person = (props) => {
-  console.log("props name", props);
-  return <li>{props.name}</li>;
+  console.log("props name and number", props);
+  return (
+    <li>
+      {props.name}
+      {props.number}
+    </li>
+  );
 };
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const addName = (event) => {
+  const addEntry = (event) => {
     event.preventDefault();
     const nameExists = persons.some((person) => person.name === newName);
     if (nameExists) {
@@ -28,24 +34,34 @@ const App = () => {
       return;
     }
     console.log("clicked button", event.target);
-    const nameObject = {
+    const newObject = {
       name: newName,
+      number: newNumber,
     };
-    setPersons(persons.concat(nameObject));
+
+    setPersons(persons.concat(newObject));
     setNewName("");
+    setNewNumber("");
   };
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
   };
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addEntry}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
