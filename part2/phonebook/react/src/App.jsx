b0eec03c533eaecfc140b60c857f4d60b871f4c0
pawelwielewickidev <pawel.wielewicki.dev@gmail.com>
2026-04-07 14:21:47 +0200

    2.11

--- part2/phonebook/react/src/App.jsx
+++ part2/phonebook/react/src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Display } from "./components/Display";
+import axios from "axios";
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchName, setSearchName] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   const addEntry = (event) => {
     event.preventDefault();
