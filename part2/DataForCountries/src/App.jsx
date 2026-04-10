abd90f9071027ee295b2dd223820b4603eba3b94
pawelwielewickidev <pawel.wielewicki.dev@gmail.com>
2026-04-10 12:04:14 +0200

    2.20

--- part2/DataForCountries/src/App.jsx
+++ part2/DataForCountries/src/App.jsx
@@ -25,6 +25,7 @@ const Display = ({ countries, setSearchCountry }) => {
           ))}
         </ul>
         <img src={country.flags.png} />
+        <CountryDetails country={country} />
       </div>
     );
   }
@@ -45,6 +46,41 @@ const Display = ({ countries, setSearchCountry }) => {
         </li>
       ))}
     </ul>
+  );
+};
+
+const CountryDetails = ({ country }) => {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    if (!country.capital) {
+      console.log("No country for weather: ", country);
+      return;
+    }
+
+    const apiKey = import.meta.env.VITE_WEATHER_KEY;
+    const capitalName = country.capital?.[0];
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${capitalName}&appid=${apiKey}&units=metric`;
+    console.log(url);
+    axios.get(url).then((response) => {
+      console.log(response.data);
+      setWeather(response.data);
+    });
+  }, [country]);
+  console.log("Weather for capital:", weather);
+  if (weather === null) {
+    return;
+  }
+  return (
+    <div>
+      <h2>Weather:</h2>
+      <p>temperature {weather.main.temp} Celsius</p>
+      <img
+        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+        alt="weather icon"
+      />
+      <p>wind {weather.wind.speed} m/s</p>
+    </div>
   );
 };
 
